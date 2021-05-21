b37474d84726254bc8776d9cb7462e067e36e6fb
Mihajlo Bisevac <kajler11@gmail.com>
2021-05-21 10:48:49 +0200

    feature: Configuration and DependencyInjection setup

--- WebScraper.CNYuan/Program.cs
+++ WebScraper.CNYuan/Program.cs
@@ -1,34 +1,44 @@
-﻿using HtmlAgilityPack;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Net;
-using System.Text;
-using System.Web;
+using System.Threading.Tasks;
 using WebScraper.CNYuan.Common;
-using WebScraper.CNYuan.Models;
 
 namespace WebScraper.CNYuan
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+            using IHost host = CreateHostBuilder(args).Build();
+
+            ServiceProvider serviceProvider = RegisterServices(args);
+            IConfiguration configuration = serviceProvider.GetService<IConfiguration>();
+            FileOutput fileOutput = new(configuration);
+
             var response = GetInitialResponse();
             var htmlDocument = response.GetHtmlDocument();
             var currencies = htmlDocument.GetCurrencies();
+
+            Extensions.Output($"{currencies.Count} currencies loaded...");
 
             foreach (var currency in currencies)
             {
                 HttpWebRequest request = CreateWebRequest(UrlConstants.BankOfChina);
                 htmlDocument = request.GetHtmlDocumentWithData(currency);
 
-                var numberOfPages = htmlDocument.GetNumberOfPages();
+                Extensions.Output($"{currency} currency record scraping in progress...");
 
                 var listOfRows = htmlDocument.GetTableRows();
+                var numberOfPages = htmlDocument.GetNumberOfPages();
 
-                if (numberOfPages > 1)
+                //if it takes too long to scrap all the data, set this to false
+                bool scrapAllPages = true;
+
+                if (scrapAllPages && numberOfPages > 1)
                 {
                     for (int pageNumber = 2; pageNumber <= numberOfPages; pageNumber++)
                     {
@@ -39,9 +49,53 @@ namespace WebScraper.CNYuan
                 }
 
                 var records = listOfRows.ToListOfRecords();
+
+                Extensions.Output($"{currency} currency record scraping finished... {records.Count} records scrapped");
+
+                if (records.Count > 0)
+                {
+                    fileOutput.Create(records);
+                }
             }
 
-            Console.WriteLine("Hello World!");
+            serviceProvider.Dispose();
+
+            await host.RunAsync();
+        }
+
+        static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostingContext, configuration) =>
+                {
+                    configuration.Sources.Clear();
+
+                    IHostEnvironment env = hostingContext.HostingEnvironment;
+
+                    configuration
+                        .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                        .AddJsonFile($"appsettings.{env.EnvironmentName}.json", true, true);
+
+                    IConfigurationRoot configurationRoot = configuration.Build();
+                });
+
+        private static IConfiguration SetupConfiguration(string[] args)
+        {
+            return new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .AddEnvironmentVariables()
+                .AddCommandLine(args)
+                .Build();
+        }
+
+        private static ServiceProvider RegisterServices(string[] args)
+        {
+            IConfiguration configuration = SetupConfiguration(args);
+            var serviceCollection = new ServiceCollection();
+
+            serviceCollection.AddSingleton(configuration);
+
+            return serviceCollection.BuildServiceProvider();
         }
 
         private static HttpWebResponse GetInitialResponse()
